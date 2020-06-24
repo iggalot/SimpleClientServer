0b2beebdff105ffc43e3fa5b5a2163a3f5f5ea2c
Jim Allen <iggalot.iggy@gmail.com>
2020-06-24 16:02:41 -0500

    WPFApplication now communicates with Server.
    Connect, Send, Receive, and Disconnect functionality separated into separate functions.

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,67 +1,53 @@
 ﻿using System;
 using System.Net.Sockets;
 
-namespace Client
+namespace ConsoleClient
 {
     public class Client
     {
-        static void Main(string[] args)
+        static public void Main(string[] args)
         {
-            Console.WriteLine("Starting Client...\r\n");
-
-            //Connect("127.0.0.1","I'm a test message");
-            Connect("10.211.55.5", "I'm a test message");
-
-            Console.WriteLine("Press any key to close window...");
-            Console.Read();
+            // Unused main function.  See GitHub initial commit for command line 
+            // implementation of this function.
         }
 
-        private static void Connect(string server, string message)
+        public static string Receive(NetworkStream stream)
         {
-            try
-            {
-                // Create a TcpClient.
-                // Note, for this client to work you need to havea TcpServer
-                // connected to the same address as specified by the server, port
-                // combination
-                Int32 port = 13000;
-                TcpClient client = new TcpClient(server, port);
+            // Receive the TcpServer response.
+            // Buffer to store the response bytes.
+            Byte[] receivedata = new byte[256];
 
-                // Translate the passed message into ASCII and store it as a Byte array
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            // String to store the response ASCII representation
+            String responseData = String.Empty;
 
-                // Get a client stream for reading and writing.
-                NetworkStream stream = client.GetStream();
+            // Read the first batch of the Tcpserver response bytes.
+            Int32 bytes = stream.Read(receivedata, 0, receivedata.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(receivedata, 0, bytes);
+            Console.WriteLine("Received: {0}", responseData);
 
-                // Send the message to the connected TcpServer.
-                stream.Write(data, 0, data.Length);
+            return responseData;
+        }
 
-                Console.WriteLine("Sent: {0}", message);
+        public static TcpClient Connect(string v, Int32 port)
+        {
+            return new TcpClient(v, port);
+        }
 
-                // Receive the TcpServer response.
-                // Buffer to store the response bytes.
-                data = new byte[256];
+        public static void Disconnect(NetworkStream stream, TcpClient client)
+        {
+            stream.Close();
+            client.Close();
+        }
 
-                // String to store the response ASCII representation
-                String responseData = String.Empty;
+        public static void Send(NetworkStream stream, string message)
+        {
+            // Translate the passed message into ASCII and store it as a Byte array
+            Byte[] senddata = System.Text.Encoding.ASCII.GetBytes(message);
 
-                // Read the first batch of the Tcpserver response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);
+            // Send the message to the connected TcpServer.
+            stream.Write(senddata, 0, senddata.Length);
 
-                // Close everything
-                stream.Close();
-                client.Close();
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine("ArgumentNullException: {0}", e);
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
+            Console.WriteLine("Sent: {0}", message);
         }
     }
 }
---
+++ ClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Windows;
+using ConsoleClient;
+
+namespace ClientWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TcpClient _client = null;
+        string server = "10.211.55.5";
+        Int32 port = 13000;
+
+        bool isConnected = false;
+
+        // Get a client stream for reading and writing.
+        NetworkStream stream;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_ConnectClick(object sender, RoutedEventArgs e)
+        {
+            string[] arg = null;
+
+            try
+            {
+                _client = Client.Connect(server, port);
+            }
+            catch (ArgumentNullException excep)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", excep);
+                return;
+            }
+            catch (SocketException excep)
+            {
+                Console.WriteLine("SocketException: {0}", excep);
+                return;
+            }
+
+            // Get a client stream for reading and writing.
+            stream = _client.GetStream();
+            isConnected = true;
+            spConnectionActive.Visibility = Visibility.Visible;
+            btnConnect.Visibility=Visibility.Collapsed;
+        }
+
+        // Actions for when the disconnect button is clicked
+        private void Button_DisconnectClick(object sender, RoutedEventArgs e)
+        {
+            if (_client.Connected)
+            {
+                // Disconnect from the stream and the socket
+                Client.Disconnect(stream, _client);
+                isConnected = false;
+                spConnectionActive.Visibility = Visibility.Collapsed;
+                btnConnect.Visibility = Visibility.Visible;
+            }
+        }
+
+        // Actions for when the send button is clicked.
+        private void Button_SendClick(object sender, RoutedEventArgs e)
+        {
+            if (_client.Connected)
+            {
+                // Send the message
+                Client.Send(stream, SendMessage.Text);
+
+                // Receive the response and display in the appropriate location
+                ReceiveMessage.Text = Client.Receive(stream);
+            } else
+            {
+                ReceiveMessage.Text = "Unable to deliver message. Client is not connected.";
+                isConnected = false;
+            }
+        }
+    }
+}
